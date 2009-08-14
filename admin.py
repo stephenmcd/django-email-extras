42acf8847cfe8e72897f54678092e51bdc1b3324
opensource@citrus.com.au <none@none>
2009-08-14 04:56:33 +0000

    --HG-- extra : convert_revision : svn%3Ae77661f8-8870-11de-b5b6-bfb8bb3c0290/trunk%402

---
+++ admin.py
@@ -0,0 +1,35 @@
+
+from django.contrib import admin
+from django import forms
+
+from email_extras.settings import USE_GNUPG, GNUPG_HOME
+
+
+if USE_GNUPG:
+	from gnupg import GPG
+	from email_extras.models import Key, Address
+	from email_extras.utils import addresses_for_key
+
+	class KeyAdmin(admin.ModelAdmin):
+	
+		def save_model(self, request, obj, form, change):
+			"""
+			import the key and parse the addresses from it and save them
+			and omit the super save_model call so as to never save the key instance
+			"""
+			gpg = GPG(gnupghome=GNUPG_HOME)
+			result = gpg.import_keys(obj.key)
+			if result.count == 0:
+				raise forms.ValidationError("Invalid Key")
+			else:
+				addresses = []
+				for key in result.results:
+					addresses.extend(addresses_for_key(gpg, key["fingerprint"]))
+				obj.addresses = ",".join(addresses)
+				for address in addresses:
+					Address.objects.get_or_create(address=address)
+
+	admin.site.register(Key, KeyAdmin)
+	admin.site.register(Address)
+
+
