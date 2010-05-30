2782c5875a012c041c3a789d65a6692230b35db3
stephenmcd <steve@jupo.org>
2010-05-30 20:24:44 +1000

    Bugfix the version number bugfix.

--- email_extras/__init__.py
+++ email_extras/__init__.py
@@ -1,16 +1,14 @@
 
-from email_extras.settings import USE_GNUPG
-
-
 __version__ = "0.1.0"
 
-if USE_GNUPG:
-    try:
-        import gnupg
-    except ImportError:
-        try:
-            from django.core.exceptions import ImproperlyConfigured
-            raise ImproperlyConfigured, "Could not import gnupg"        except ImportError:
-            pass
 
+try:
+    from email_extras.settings import USE_GNUPGexcept ImportError:
+    pass
+else:
+    if USE_GNUPG:
+        try:
+            import gnupg
+        except ImportError:
+            raise ImproperlyConfigured, "Could not import gnupg"
 
