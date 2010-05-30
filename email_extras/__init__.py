a63481c0198791b41cf377e4bc8247afaed90364
stephenmcd <steve@jupo.org>
2010-05-30 20:20:52 +1000

    Fix to allow version number to be imported without dependencies being installed.

--- email_extras/__init__.py
+++ email_extras/__init__.py
@@ -1,13 +1,16 @@
 
-from django.core.exceptions import ImproperlyConfigured
-
 from email_extras.settings import USE_GNUPG
 
 
 __version__ = "0.1.0"
 
 if USE_GNUPG:
-	try:
-		import gnupg
-	except ImportError:
-		raise ImproperlyConfigured, "Could not import gnupg"
+    try:
+        import gnupg
+    except ImportError:
+        try:
+            from django.core.exceptions import ImproperlyConfigured
+            raise ImproperlyConfigured, "Could not import gnupg"        except ImportError:
+            pass
+
+
