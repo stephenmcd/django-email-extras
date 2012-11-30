93780435b6c9c2df30eec27fc6fe243dd22a3896
Stephen McDonald <steve@jupo.org>
2012-11-30 17:54:01 +1100

    Move gnupg check into settings so that setup.py can run against Django 1.5

--- email_extras/__init__.py
+++ email_extras/__init__.py
@@ -1,16 +1,2 @@
 
 __version__ = "0.1.8"
-
-
-try:
-    from email_extras.settings import USE_GNUPG
-except ImportError:
-    pass
-else:
-    if USE_GNUPG:
-        try:
-            import gnupg
-        except ImportError:
-            raise ImproperlyConfigured, "Could not import gnupg"
-
-
--- email_extras/settings.py
+++ email_extras/settings.py
@@ -5,3 +5,10 @@ from django.conf import settings
 GNUPG_HOME = getattr(settings, "EMAIL_EXTRAS_GNUPG_HOME", None)
 USE_GNUPG = getattr(settings, "EMAIL_EXTRAS_USE_GNUPG", GNUPG_HOME is not None)
 ALWAYS_TRUST = getattr(settings, "EMAIL_EXTRAS_ALWAYS_TRUST_KEYS", False)
+
+
+if USE_GNUPG:
+    try:
+        import gnupg
+    except ImportError:
+        raise ImproperlyConfigured, "Could not import gnupg"
