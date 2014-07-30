53f1e25f308fcc6b73bc6954cd8aa6b9b2977e48
Stephen McDonald <steve@jupo.org>
2014-07-30 19:54:10 +1000

    Python 3 classifiers

--- setup.py
+++ setup.py
@@ -1,5 +1,18 @@
 
-from setuptools import setup
+import sys
+from shutil import rmtree
+from setuptools import setup, find_packages
+
+
+if sys.argv[:2] == ["setup.py", "bdist_wheel"]:
+    # Remove previous build dir when creating a wheel build,
+    # since if files have been removed from the project,
+    # they'll still be cached in the build dir and end up
+    # as part of the build, which is unexpected.
+    try:
+        rmtree("build")
+    except:
+        pass
 
 
 setup(
@@ -12,7 +25,7 @@ setup(
                   "the Django templating system.",
     long_description = open("README.rst").read(),
     url = "https://github.com/stephenmcd/django-email-extras",
-    packages = ["email_extras",],
+    packages = find_packages(),
     zip_safe = False,
     include_package_data = True,
     install_requires=["python-gnupg", "sphinx-me",],
@@ -22,6 +35,10 @@ setup(
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
         "Framework :: Django",
         "Topic :: Communications :: Email",
         "Topic :: Security :: Cryptography",
