606efba1e228cd91acc866457413c6593c81f286
Tim Heithecker <tim.heithecker@gmail.com>
2016-11-16 13:03:44 +0100

    failed encryption handling

--- email_extras/utils.py
+++ email_extras/utils.py
@@ -7,7 +7,7 @@ from django.utils import six
 from django.utils.encoding import smart_text
 
 from email_extras.settings import (USE_GNUPG, GNUPG_HOME, ALWAYS_TRUST,
-                                   GNUPG_ENCODING, ENCRYPTION_FAILED_MESSAGE)
+                                   GNUPG_ENCODING, FAILED_ENCRYPTION_BODY)
 
 
 if USE_GNUPG:
@@ -27,11 +27,11 @@ def addresses_for_key(gpg, key):
     return addresses
 
 
-def get_body_for_failed_encryption(body, addr):
+def get_failed_encryption_body(body, addr):
     """
     Meant to be monkey patched if required.
     """
-    return ENCRYPTION_FAILED_MESSAGE
+    return FAILED_ENCRYPTION_BODY
 
 
 def send_mail(subject, body_text, addr_from, addr_to, fail_silently=False,
@@ -68,7 +68,7 @@ def send_mail(subject, body_text, addr_from, addr_to, fail_silently=False,
             encrypted = gpg.encrypt(body, addr_list[0],
                                     always_trust=ALWAYS_TRUST)
             if encrypted == "" and body != "":  # encryption failed
-                return get_body_for_failed_encryption(body, addr_list[0])
+                return get_failed_encryption_body(body, addr_list[0])
             return smart_text(encrypted)
         return body
 
